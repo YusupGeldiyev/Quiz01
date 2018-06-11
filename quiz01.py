632581bd45d3b0ca524770d70e878f744db40f89
YusupGeldiyev <40005122+YusupGeldiyev@users.noreply.github.com>
2018-06-11 20:27:40 +0000

    Update quiz01.py

--- quiz01.py
+++ quiz01.py
@@ -1,15 +1,18 @@
 #matVec takes a matrix and a vector and returns the corresponding matrix-vector multiply
 def matVec(matrix,vector):
   '''
-This function takes a matrix and a vector as its arguments. It then gets a new vector 
-before retruning the now updated vector.
+This function takes a matrix and a vector as its arguments. then it creates array and It then gets a new vector 
+by multiplying given matrix and given vector. finally, it returns the result back to array after calculation
   ''' 
   result = []
   for i in range(len(matrix)):
+    # runs for the raws of new vector
       total = 0    
       for j in range(len(vector)):
+        #runs for the columns of new vector
         total += matrix[i][j] * vector[j]
   result.append(total)
+  # the amount of columns in the matrix hast to be the amount as the raws in the vector
   return result
 
 matrix1 = [[2,3],[4,5],[5,6]]
